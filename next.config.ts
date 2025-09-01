469d8382ac7f9ee435abb46f2d1c5786038be16c
septirizky <septirizky79@gmail.com>
2025-09-01 09:23:38 +0700

    add aws s3

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
   output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mbh-storage.s3.ap-southeast-2.amazonaws.com",
+        pathname: "/netflix100/profile-pictures/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
