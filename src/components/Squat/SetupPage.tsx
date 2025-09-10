90320d98ed369f5386aa4a4f9842fa2e56a33d9d
AnnisaRizki30 <annisarizkililiandari@gmail.com>
2025-09-10 11:56:12 +0700

    push update code

--- src/components/Squat/SetupPage.tsx
+++ src/components/Squat/SetupPage.tsx
@@ -88,7 +88,7 @@ const SetupPage: React.FC<SetupPageProps> = ({
           marginBottom: 0
         }}
       >
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center gap-8">
           <div className="flex items-center gap-2">
             <div className={`w-5 h-5 rounded-full border-2 mb-2 flex items-center justify-center ${webcamRunning ? 'border-[#00FF51]' : 'border-[#FFFFFF]'}`}>
               {webcamRunning ? <Check size={12} className="text-[#00FF51]" /> : <X size={12} className="text-[#FFFFFF]" />}
