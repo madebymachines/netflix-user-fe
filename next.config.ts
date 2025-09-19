8e87f44f9edc5d5dcb28cadce1469ea570a633dd
septirizky <septirizky79@gmail.com>
2025-09-19 10:37:31 +0700

    change api be

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,16 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Konfigurasi yang sudah ada (TETAP DIPERTAHANKAN)
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  output: 'standalone',
+  output: "standalone",
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'mbh-storage.s3.ap-southeast-2.amazonaws.com',
-        pathname: '/netflix100/profile-pictures/**',
+        protocol: "https",
+        hostname: "mbh-storage.s3.ap-southeast-2.amazonaws.com",
+        pathname: "/netflix100/profile-pictures/**",
       },
     ],
   },
@@ -23,11 +23,11 @@ const nextConfig: NextConfig = {
       {
         // 'source' harus cocok persis dengan baseURL di file axios.ts Anda.
         // Ini akan menangkap semua permintaan yang dimulai dengan /api/v1/...
-        source: '/api/v1/:path*',
+        source: "/api/v1/:path*",
 
         // 'destination' adalah URL backend Anda di Render.
         // Next.js akan mengganti `:path*` dengan sisa URL dari 'source'.
-        destination: 'https://netflix-be-1.onrender.com/v1/:path*',
+        destination: "https://mirror-100-plus-be.onrender.com/v1/:path*",
       },
     ];
   },
